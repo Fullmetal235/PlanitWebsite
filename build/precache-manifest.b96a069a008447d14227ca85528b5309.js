7b885e75d81f0e37925e2432b24c59c8c92e0f36
Fullmetal235 <jjbillupsbrown@gmail.com>
2023-11-18 09:20:29 -0600

    New update

---
+++ build/precache-manifest.b96a069a008447d14227ca85528b5309.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8cbc72e85376dec4186",
+    "url": "/static/css/main.fa0d0d3d.chunk.css"
+  },
+  {
+    "revision": "d8cbc72e85376dec4186",
+    "url": "/static/js/main.d8cbc72e.chunk.js"
+  },
+  {
+    "revision": "2875ac0c00ccdd31eb70",
+    "url": "/static/js/1.2875ac0c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3f3aa6b05ec044bba10306805530eac0",
+    "url": "/index.html"
+  }
+];
